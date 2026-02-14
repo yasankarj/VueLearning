65d1de171c0e8cadd63392f80c4380fd9c971a1c
yasankarj <yasankarj@gmail.com>
2026-02-14 14:23:14 +0530

    lesson: watchers and computed properties

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -2,11 +2,44 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName: ''
+      // fullName: ''
     };
   },
+  watch: {
+    counter(value) {
+      if (value > 50 || value < 0) {
+        this.counter = 0;
+      }
+    }
+    //   name(value) {
+    //     if (value === '')
+    //       this.fullName = ''
+    //     else
+    //       this.fullName = value + ' ' + this.lastName
+    //   },
+    //   lastName(value) {
+    //     if (value === '')
+    //       this.fullName = ''
+    //     else
+    //       this.fullName = this.name + ' ' + value
+    //   }
+  },
+  computed: {
+    fullName() {
+      if (this.name === '' || this.lastName === '')
+        return ''
+      return this.name + ' ' + this.lastName
+    }
+  },
   methods: {
-    setName(event, lastName) {
+    outputFullname(lastName) {
+      if (this.name === '')
+        return ''
+      return this.name + ' ' + lastName
+    },
+    setName(event) {
       this.name = event.target.value;
     },
     add(num) {
@@ -17,7 +50,8 @@ const app = Vue.createApp({
       // this.counter--;
     },
     resetInput() {
-      this.name = ''
+      this.name = '';
+      this.lastName = '';
     }
   }
 });
