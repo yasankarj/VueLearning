05cad51a9f260c600df2e9381b4b9f1a2286f9a1
yasankarj <yasankarj@gmail.com>
2026-02-27 17:19:38 +0530

    lesson: first app with vue dev server

---
+++ vue-first-app/src/components/FriendContact.vue
@@ -0,0 +1,57 @@
+<template>
+    <li>
+        <h2>{{ name }} {{ friendIsFavourite ? '(⭐⭐⭐)' : '' }}</h2>
+        <button @click="toggleFavorites">Make Favourite</button>
+        <button @click="toggleDetails">{{detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+            <li><strong>Email:</strong> {{ emailAddress }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    // props: ['name', 'phoneNumber', 'emailAddress', 'isFavourite'],
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        phoneNumber: {
+            type: String,
+            required: true
+        },
+        emailAddress: {
+            type: String,
+            required: true
+        },
+        isFavourite: {
+            type: Boolean,
+            required: false,
+            default: false,
+            // validator: function(value) {
+            //     return value === '1' || value === '0'
+            // }
+        }
+    },
+    data() {
+        return {
+            detailsAreVisible: false,
+            friendIsFavourite: this.isFavourite
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        },
+        toggleFavorites() {
+            if (this.friendIsFavourite === '1') {
+                this.friendIsFavourite = '0';
+            } else {
+                this.friendIsFavourite = '1';
+            }
+        }
+    }
+}
+</script>
